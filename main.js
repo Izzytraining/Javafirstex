6c32f489d1ceaf262d33edd87947fd8ac1a244df
Izzytraining <izzyjhalliday23@gmail.com>
2022-12-10 12:39:15 +0000

    changes

--- main.js
+++ main.js
@@ -13,11 +13,6 @@ console.log(bodmasExample);
 const randomnum = math.floor(math.random()* 6 +1);
 console.log(randomnum);
 
-//concatination 
-const fullname = firstname + lastname;
-console.log(fullname);
-
-//concatination numbers and strings
 
 
 
