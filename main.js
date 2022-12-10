4de0137a806837436fa1f275a1f72283fcd0ed44
Izzytraining <izzyjhalliday23@gmail.com>
2022-12-10 09:27:25 +0000

    addchanges

---
+++ main.js
@@ -0,0 +1,26 @@
+let age = 60
+console.log(age)
+
+let numberofRabbits = 2;
+
+numberofRabbits - numberofRabbits * 3
+console.log(numberofRabbits);
+
+let bodmasExample  = 3 
+bodmasExample = (bodmasExample + 5) / 6;
+console.log(bodmasExample); 
+
+const randomnum = math.floor(math.random()* 6 +1);
+console.log(randomnum);
+
+//concatination 
+const fullname = firstname + lastname;
+console.log(fullname);
+
+//concatination numbers and strings
+
+
+
+
+
+
